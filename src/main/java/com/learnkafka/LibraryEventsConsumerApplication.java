8934063600e4755f8bbcbfea4d90bd643dec8b43
Amarpreet <amarpreetbhatia@gmail.com>
2021-06-01 23:33:51 +1000

    Initial setup for kafka Listener, that uses ConcurrentKafkaContainerListener Behind the scene.

--- src/main/java/com/learnkafka/LibraryEventsConsumerApplication.java
+++ src/main/java/com/learnkafka/LibraryEventsConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.learnkafka.libraryeventsconsumer;
+package com.learnkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
