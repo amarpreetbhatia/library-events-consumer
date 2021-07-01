7235b9412915d88a2d3f3977a2a2002b93d2e275
Amarpreet <amarpreetbhatia@gmail.com>
2021-07-01 13:33:33 +1000

    Added springdoc for swagger specs, it can access by url ```http://localhost:8081/swagger-ui.html```

--- src/main/java/com/learnkafka/LibraryEventsConsumerApplication.java
+++ src/main/java/com/learnkafka/LibraryEventsConsumerApplication.java
@@ -1,13 +1,31 @@
 package com.learnkafka;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class LibraryEventsConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryEventsConsumerApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Apache Kafka Consumer API")
+						.description("Spring boot sample application")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation()
+						.description("SpringShop Wiki Documentation")
+						.url("https://springshop.wiki.github.org/docs"));
+	}
 }
--- src/main/java/com/learnkafka/controller/DummyController.java
+++ src/main/java/com/learnkafka/controller/DummyController.java
@@ -1,6 +1,9 @@
 package com.learnkafka.controller;
 
 import com.learnkafka.config.BasicConfigurationProperties;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,13 @@ public class DummyController {
     @Autowired
     BasicConfigurationProperties basicConfigurationProperties;
 
+    @Operation(summary = "This to get the basic value from Property files")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",description=" Get a String Value")
+            }
+    )
     @GetMapping("/basic/values")
     public String showBasicPropertyValue(){
         log.info("Printing basic value1 {} that was written on {}",
